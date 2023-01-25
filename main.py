15d89cb042f0aa37d1f9048c0e88dd9fb8b6cfa4
Joseph Masone <joemasone050897@gmail.com>
2023-01-24 19:38:39 -0500

    add get_wufoo_data()

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
+import sys
+import requests
+from secrets import wufoo_key
+from requests.auth import HTTPBasicAuth
 
-def main():
-    print('Hello World')
+
+def get_wufoo_data() -> list[dict[1]]:
+    url = "https://j1masone.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
+    response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
+
+    if response.status_code != 200:  # if we don't get an ok response we have trouble
+        print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
+        sys.exit(-1)
+
+    json_response = response.json()
+    return json_response
 
 
 if __name__ == '__main__':
-    main()
+    form_entries = get_wufoo_data()
+
+    print(form_entries)
+
 
