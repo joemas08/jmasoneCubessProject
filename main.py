c6768a35072cd46832d84a460bab1a47ffac2a61
Joseph Masone <joemasone050897@gmail.com>
2023-01-25 19:49:20 -0500

    add send_form_to_file()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import requests
-from secrets import wufoo_key
+from secret import wufoo_key
 from requests.auth import HTTPBasicAuth
 
 
@@ -8,7 +8,7 @@ def get_wufoo_data() -> list[dict[1]]:
     url = "https://j1masone.wufoo.com/api/v3/forms/cubes-project-proposal-submission/entries/json"
     response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
 
-    if response.status_code != 200:  # if we don't get an ok response we have trouble
+    if response.status_code != 200:
         print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
         sys.exit(-1)
 
@@ -16,9 +16,14 @@ def get_wufoo_data() -> list[dict[1]]:
     return json_response
 
 
+def send_form_data_to_file(form_passed: list[dict]):
+    for lists, entries in form_passed.items():
+        for entry in entries:
+            for field, data in entry.items():
+                print(f'{field}:{data}')
+            print('-' * 50)
+
+
 if __name__ == '__main__':
     form_entries = get_wufoo_data()
-
-    print(form_entries)
-
-
+    send_form_data_to_file(form_entries)
