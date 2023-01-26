dd2cf880d7b3df55ab8c6fc9beff3280eefbc2f1
Joseph Masone <joemasone050897@gmail.com>
2023-01-26 16:31:28 -0500

    add handle_request_functions.py, file_functions.py, refactor main.py

---
+++ handle_request_functions.py
@@ -0,0 +1,30 @@
+import requests
+from requests.auth import HTTPBasicAuth
+from secret import wufoo_key
+import sys
+
+
+def get_wufoo_data(url_passed: str) -> list[dict[1]]:
+    try:
+        response = requests.get(url_passed,
+                                auth=HTTPBasicAuth(wufoo_key, 'pass'))
+    except requests.exceptions.InvalidURL:
+        print(f'The URL {url_passed} had: Invalid URL Error')
+        exit()
+    except requests.exceptions.ConnectionError:
+        print(f'The URL {url_passed} had: Connection Error')
+        exit()
+
+    _check_status_code(response)
+
+    json_response = response.json()
+    return json_response
+
+
+def _check_status_code(response: requests.Response):
+    if response.status_code != 200:
+        print(f'Failed to get data, response code: {response.status_code} '
+              f'and error message: {response.reason}')
+        sys.exit(-1)
+    else:
+        pass
--- main.py
+++ main.py
@@ -1,41 +1,9 @@
-import sys
-import requests
-from secret import wufoo_key
-from requests.auth import HTTPBasicAuth
-
-
-def get_wufoo_data() -> list[dict[1]]:
-    url = ("https://j1masone.wufoo.com/api/v3/forms/cubes-project-proposal-"
-           "submission/entries/json")
-    response = requests.get(url, auth=HTTPBasicAuth(wufoo_key, 'pass'))
-
-    if response.status_code != 200:
-        print(f"Failed to get data, response code:{response.status_code}"
-              "and error message: {response.reason} ")
-        sys.exit(-1)
-
-    json_response = response.json()
-    return json_response
-
-
-def send_form_data_to_file(form_passed: list[dict], file_passed: str):
-    clear_file(file_passed)
-    for lists, entries in form_passed.items():
-        for entry in entries:
-            for field, data in entry.items():
-                if data == '':
-                    data = 'No entry'
-                with open(file_passed, 'a') as file:
-                    file.write(f'{field}\t{data}\n')
-            with open('entries.txt', 'a') as file:
-                file.write('-' * 50 + '\n')
-
-
-def clear_file(file_passed):
-    with open(file_passed, 'a', encoding='UTF-8') as file:
-        file.truncate(0)
+from handle_request_functions import get_wufoo_data
+from file_functions import send_form_data_to_file
 
 
 if __name__ == '__main__':
-    form_entries = get_wufoo_data()
+    url = ("https://j1masone.wufoo.com/api/v3/forms/cubes-project-proposal-"
+           "submission/entries/json")
+    form_entries = get_wufoo_data(url)
     send_form_data_to_file(form_entries, 'entries.txt')
